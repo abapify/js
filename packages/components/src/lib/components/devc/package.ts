fd1b773f51f0a660c925f8d4026ea0ff11b77cb9
Petr Plenkov <petr.plenkov@gmail.com>
2024-11-07 11:12:20 +0000

    format

--- packages/components/src/lib/components/devc/package.ts
+++ packages/components/src/lib/components/devc/package.ts
@@ -1,18 +1,18 @@
-import { Component } from "../component"
+import { Component } from '../component';
 
 interface PackageInput {
-    description: string
+  description: string;
 }
 
 export class Package extends Component<PackageInput> {
-    override get abapgitSerializer(): string {
-        return "LCL_OBJECT_DEVC"
-    }
-    override toAbapgit(): unknown {
-        return {
-            DEVC: {
-                CTEXT: this.input.description
-            }
-        }
-    }
+  override get abapgitSerializer(): string {
+    return 'LCL_OBJECT_DEVC';
+  }
+  override toAbapgit(): unknown {
+    return {
+      DEVC: {
+        CTEXT: this.input.description,
+      },
+    };
+  }
 }
