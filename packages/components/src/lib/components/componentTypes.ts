97c53fb6affea04fdc37ddf76545a600b75bb74c
Petr Plenkov <petr.plenkov@gmail.com>
2024-11-11 10:35:06 +0000

    format

--- packages/components/src/lib/components/componentTypes.ts
+++ packages/components/src/lib/components/componentTypes.ts
@@ -1,4 +1,4 @@
 export enum ComponentType {
-    dataElement = 'DTEL',
-    domain = 'DOMA',
+  dataElement = 'DTEL',
+  domain = 'DOMA',
 }
--- samples/cds/src/lib/objectList.ts
+++ samples/cds/src/lib/objectList.ts
@@ -1,14 +1,14 @@
-import { ObjectTypes } from "./objectTypes"
-import { Domain, DataElement } from "@abapify/components"
+import { ObjectTypes } from './objectTypes';
+import { Domain, DataElement } from '@abapify/components';
 
-export type ObjectList = Array<DomainItem | DataElementItem>
+export type ObjectList = Array<DomainItem | DataElementItem>;
 
 interface DomainItem {
-    type: ObjectTypes.domain,
-    domain: Domain
+  type: ObjectTypes.domain;
+  domain: Domain;
 }
 
 interface DataElementItem {
-    type: ObjectTypes.dataElement,
-    dataElement: DataElement
+  type: ObjectTypes.dataElement;
+  dataElement: DataElement;
 }
