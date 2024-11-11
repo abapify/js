5007da22235559fc5b24c68e809184eb9a33f9d1
Petr Plenkov <petr.plenkov@gmail.com>
2024-11-11 10:34:44 +0000

    abap components: initial commit

--- packages/components/src/lib/components/component.ts
+++ packages/components/src/lib/components/component.ts
@@ -1,4 +1,5 @@
 import { toAbapGitXML } from '../abapgit/abapgit';
+import { ComponentType } from './componentTypes';
 
 export abstract class Component<T> {
   input: T;
@@ -7,6 +8,8 @@ export abstract class Component<T> {
   }
   abstract toAbapgit(): unknown;
   abstract get abapgitSerializer(): string;
+  abstract get type(): ComponentType;
+  abstract get id(): string;
   toAbapgitXML(): string {
     return toAbapGitXML(this.toAbapgit(), this.abapgitSerializer);
   }
---
+++ samples/cds/README.md
@@ -0,0 +1,7 @@
+# samples/cds
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test samples/cds` to execute the unit tests via [Vitest](https://vitest.dev/).
