1919738328554132b9d6d077998e5c44cc504086
Petr Plenkov <petr.plenkov@gmail.com>
2024-11-13 23:43:08 +0000

    format

--- samples/adt/objects/doma/domain.ts
+++ samples/adt/objects/doma/domain.ts
@@ -1,29 +1,29 @@
-import { Component } from "../../common/component";
+import { Component } from '../../common/component';
 // Namespace: doma/content
 interface DomaContent {
-    typeInformation: {
-        datatype: string;
-        length: number;
-        decimals: number;
-    },
-    outputInformation: {
-        length: number;
-        style: string;
-        conversionExit?: string;
-        signExists: boolean;
-        lowercase: boolean;
-        ampmFormat: boolean;
-    };
-    valueInformation: {
-        valueTableRef?: string;
-        appendExists: boolean;
-        fixValues: {
-            position: number;
-            low: string;
-            high?: string;
-            text: string;
-        }[];
-    };
+  typeInformation: {
+    datatype: string;
+    length: number;
+    decimals: number;
+  };
+  outputInformation: {
+    length: number;
+    style: string;
+    conversionExit?: string;
+    signExists: boolean;
+    lowercase: boolean;
+    ampmFormat: boolean;
+  };
+  valueInformation: {
+    valueTableRef?: string;
+    appendExists: boolean;
+    fixValues: {
+      position: number;
+      low: string;
+      high?: string;
+      text: string;
+    }[];
+  };
 }
 
-export type Domain = Component<{ content: DomaContent }>
+export type Domain = Component<{ content: DomaContent }>;
