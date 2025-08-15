21631d0f877037bfda3dbe1e728e4b976bb4831e
Petr Plenkov <petr.plenkov@gmail.com>
2025-08-15 12:37:00 +0000

    feat: implement OAuth token fetching and validation with comprehensive tests

--- packages/sk/jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  displayName: 'sk',
-  preset: '../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/sk',
-};
