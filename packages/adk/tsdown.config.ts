586c84529137508cbf678390b377d83de3747fa7
Petr Plenkov <petr.plenkov@gmail.com>
2025-09-15 15:12:31 +0000

    build: configure tsdown bundler and optimize package dependencies

--- packages/adk/tsdown.config.ts
+++ packages/adk/tsdown.config.ts
@@ -2,7 +2,10 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  sourcemap: true,
+  sourcemap: false,
   tsconfig: 'tsconfig.lib.json',
   skipNodeModulesBundle: true,
+  external: ['fast-xml-parser', 'fxmlp', 'vitest'],
+  dts: false,
+  minify: false,
 });
