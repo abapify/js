695406deafb08f3a796c50e9b1608e5555b69c37
Petr Plenkov <petr.plenkov@gmail.com>
2025-11-26 16:26:41 +0100

    ``` docs: add package.json best practices to nx-monorepo guide and update abapify submodule
    - Document that devDependencies should be hoisted to root package.json
    - Explain why npm scripts should be replaced with Nx targets
    - Add minimal package.json template showing correct structure
    - Include examples of common mistakes (adding tsdown, tsx, typescript as devDeps)
    - Show how to define custom commands in project.json instead of package.json scripts
    - Update abapify submodule to c80a062
    ```

---
+++ packages/adt-schemas-xsd/src/index.ts
@@ -0,0 +1,30 @@
+/**
+ * ADT XML Schemas - Auto-generated from SAP XSD definitions
+ */
+
+export { default as adtcore } from './schemas/adtcore';
+export { default as atom } from './schemas/atom';
+export { default as xml } from './schemas/xml';
+export { default as abapsource } from './schemas/abapsource';
+export { default as abapoo } from './schemas/abapoo';
+export { default as classes } from './schemas/classes';
+export { default as interfaces } from './schemas/interfaces';
+export { default as atc } from './schemas/atc';
+export { default as atcresult } from './schemas/atcresult';
+export { default as atcresultquery } from './schemas/atcresultquery';
+export { default as atcfinding } from './schemas/atcfinding';
+export { default as atcobject } from './schemas/atcobject';
+export { default as atctagdescription } from './schemas/atctagdescription';
+export { default as atcinfo } from './schemas/atcinfo';
+export { default as atcworklist } from './schemas/atcworklist';
+export { default as transportmanagment } from './schemas/transportmanagment';
+export { default as checkrun } from './schemas/checkrun';
+export { default as transportsearch } from './schemas/transportsearch';
+export { default as checklist } from './schemas/checklist';
+export { default as debugger } from './schemas/debugger';
+export { default as logpoint } from './schemas/logpoint';
+export { default as traces } from './schemas/traces';
+export { default as quickfixes } from './schemas/quickfixes';
+export { default as log } from './schemas/log';
+
+export { parse, build, type InferXsd, type XsdSchema } from 'ts-xsd';
