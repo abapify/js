f9f8b4b8a59efe120447da1f814fdc021374c4d7
Petr Plenkov <petr.plenkov@gmail.com>
2025-12-15 11:26:32 +0100

    refactor: remove adt-plugin-atc package
    ATC functionality will be implemented directly in adt-cli commands,
    no need for a separate plugin package.

--- packages/adt-plugin-atc/src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @abapify/adt-plugin-atc
- * 
- * ABAP Test Cockpit (ATC) plugin for abapify
- */
-
-export { AtcPlugin } from './plugin';
-export type { AtcCheckOptions, AtcFinding, AtcRunResult, AtcVariant } from './types';
