ba6b3754b6d7f313dbde47a400cd533cd1da7b7c
Petr Plenkov <petr.plenkov@booking.com>
2025-12-15 11:23:28 +0100

    feat(atc): initial ATC plugin package structure
    - Add @abapify/adt-plugin-atc package
    - Define types for ATC checks, findings, variants
    - Stub plugin implementation
    - Configure build with tsdown

---
+++ packages/adt-plugin-atc/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @abapify/adt-plugin-atc
+ * 
+ * ABAP Test Cockpit (ATC) plugin for abapify
+ */
+
+export { AtcPlugin } from './plugin';
+export type { AtcCheckOptions, AtcFinding, AtcRunResult, AtcVariant } from './types';
