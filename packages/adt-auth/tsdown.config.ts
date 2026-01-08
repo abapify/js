4992dd14809153668b806d2ab04f10676c85caa4
Petr Plenkov <petr.plenkov@gmail.com>
2026-01-08 14:50:52 +0100

    fix(adt-auth): reference tsconfig.lib.json in tsdown config

--- packages/adt-auth/tsdown.config.ts
+++ packages/adt-auth/tsdown.config.ts
@@ -4,4 +4,5 @@ import baseConfig from '../../tsdown.config.ts';
 export default defineConfig({
   ...baseConfig,
   entry: ['src/index.ts', 'src/plugins/basic.ts'],
+  tsconfig: 'tsconfig.lib.json',
 });
